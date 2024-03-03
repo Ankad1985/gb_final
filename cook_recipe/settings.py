4a0f8a123aa666fab472a4890670569505060822
Andrei <kadyrov_andrei@mail.ru>
2024-03-03 09:39:25 +0300

    return media and static

--- cook_recipe/settings.py
+++ cook_recipe/settings.py
@@ -34,7 +34,7 @@ if os.environ.get("DEBUG") == "False":
     DEBUG = False
 else:
     DEBUG = True
-    
+
 SESSION_COOKIE_SECURE = True  
 CSRF_COOKIE_SECURE = True
 
@@ -166,11 +166,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"  
-STATIC_ROOT = BASE_DIR / "static/"  
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
-MEDIA_URL = "media/"  
-MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'website/media'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
